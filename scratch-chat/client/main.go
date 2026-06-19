9d925036f45590723089289dc59bd975d90bd0d1
LUCKYHARSHA001 <harshaalla7@gmail.com>
2026-06-19 20:42:26 +0530

    server setup for bidirectional streaming

--- scratch-chat/client/main.go
+++ scratch-chat/client/main.go
@@ -1,1 +1,5 @@
-package client
+package main 
+
+import (
+	
+)
--- scratch-chat/server/main.go
+++ scratch-chat/server/main.go
@@ -1,1 +1,59 @@
-package server
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"bufio"
+	"net"
+	"google.golang.org/grpc"
+	pb "scratch-chat/proto"
+)
+
+type server struct{
+	pb.UnimplementedChatServiceServer
+}
+func (s *server) StreamChat(stream pb.ChatService_StreamChatServer)error{
+	log.Println("Connected! to Bidirectional stream")
+
+	go func(){
+		scanner:=bufio.NewScanner(os.Stdin)
+		for scanner.Scan(){
+			text:=scanner.Text()
+			if text==""{
+				continue
+			}
+			stream.Send(&pb.Response{
+				Server:"server",
+				Text: text,
+			})
+			fmt.Print("Server > ")
+		}
+	}()
+
+	for{
+		req,err:=stream.Recv()
+		if err != nil {
+			log.Printf("\nError receiving from client: %v", err)
+			return err
+		}
+		fmt.Printf("\n[%s]: %s\nServer > ", req.Client, req.Text)
+		os.Stdout.Sync()
+	}
+
+
+}
+
+func main(){
+	lis,err:=net.Listen("tcp",":5000")
+	if err!=nil{
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	grpcServer:=grpc.NewServer()
+	pb.RegisterChatServiceServer(grpcServer,&server{})
+	log.Println("Server is running on port 5000")
+	fmt.Print("Server > ")
+	if err := grpcServer.Serve(lis); err != nil {
+    log.Fatalf("Failed to serve: %v", err)
+}
+}
